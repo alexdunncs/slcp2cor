a2ad7a788e95090a6ae7b48f0dfc46fdf22e9fa5
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2020-11-09 08:34:48 -0800

    Updated geo_with_ll.py to current version

--- script/OLD_geo_with_ll.py
+++ script/OLD_geo_with_ll.py
@@ -13,7 +13,6 @@ import argparse
 import datetime
 import numpy as np
 from scipy.interpolate import griddata
-from scipy.signal import convolve2d as conv2
 
 import isce
 import isceobj
@@ -105,15 +104,7 @@ def cmdLineParse():
             help = 'latitude file.')
     parser.add_argument('-lon', dest='lon', type=str, required=True,
             help = 'longitude file')
-    parser.add_argument('-bbox', dest='bbox', type=str, required=False,
-            help='geocode bounding box (format: S/N/W/E). or you can input master frame pickle file that contains a bounding box')
-    parser.add_argument('-latMin', dest='latMin', type=float, required=False,default=0,
-            help='geocode bounding box (format: S/N/W/E). or you can input master frame pickle file that contains a bounding box')
-    parser.add_argument('-latMax', dest='latMax', type=float, required=False,default=0,
-            help='geocode bounding box (format: S/N/W/E). or you can input master frame pickle file that contains a bounding box')
-    parser.add_argument('-lonMin', dest='lonMin', type=float, required=False,default=0,
-            help='geocode bounding box (format: S/N/W/E). or you can input master frame pickle file that contains a bounding box')
-    parser.add_argument('-lonMax', dest='lonMax', type=float, required=False,default=0,
+    parser.add_argument('-bbox', dest='bbox', type=str, required=True,
             help='geocode bounding box (format: S/N/W/E). or you can input master frame pickle file that contains a bounding box')
     parser.add_argument('-ssize', dest='ssize', type=float, default=1.0,
             help = 'output sample size. default: 1.0 arcsec')
@@ -135,7 +126,6 @@ if __name__ == '__main__':
     length = inImage.length
     scheme = inImage.scheme
     nbands = inImage.bands
-    print("number of bands : {}".format(nbands))
     datatype = inImage.dataType
 
     #check file width and length
@@ -168,16 +158,15 @@ if __name__ == '__main__':
     else:
         raise Exception('unknow resampling method!')
     print("interpolation method: {}".format(rmethod))
-     
-    if inps.latMin!=0 and inps.latMax!=0 and inps.lonMin!=0 and inps.lonMax!=0:
-         bbox = ( inps.latMin, inps.latMax, inps.lonMin, inps.lonMax )
-    else: 
-         bbox = (0,0,0,0)
-#    bbox[1] = inps.latMax
- #   bbox[2] = inps.lonMin
- #   bbox[3] = inps.lonMax
 
-
+    if os.path.isfile(inps.bbox):
+        with open(inps.bbox, 'rb') as f:
+            frame = pickle.load(f)
+        bbox = frame.snwe
+    else:
+        bbox = [float(val) for val in inps.bbox.split('/')]
+        if len(bbox) != 4:
+            raise Exception('bbox should contain 4 floating point values!')
     print("geocode bounding box:")
     print("south: {}".format(bbox[0]))
     print("north: {}".format(bbox[1]))
@@ -186,96 +175,28 @@ if __name__ == '__main__':
 
     #get input bands saved in a list
     print("reading data to be geocoded")
-    print("{}   {}   {}".format(width,length,datatype))
     bands = read_bands(inps.input, length, width, scheme, nbands, datatype)
 
     #get latitude
     print("reading latitude")
     lat = read_bands(inps.lat, length, width, latImage.scheme, latImage.bands, latImage.dataType)
     lat = lat[0]
-    lat2= np.asarray(lat)
-    lat2[lat2==0]=np.nan
-    lat2=np.reshape(lat2,[length,width])
-    win2=np.ones((5,5))/25
-    lat2m=conv2(lat2,win2,'same')
-    lat2m=lat2m.flatten()
-    lat2=lat2.reshape(length*width)
-    print("{}".format(np.min(lat2)))
     #get longitude
     print("reading longitude")
     lon = read_bands(inps.lon, length, width, lonImage.scheme, lonImage.bands, lonImage.dataType)
     lon = lon[0]
-    lon2=lon
-    lon2[lon2==0]=np.nan
-    print("{}".format(np.min(lon2)))
-#    if inps.latMin==0 and inps.latMax==0 and inps.lonMin==0 and inps.lonMax==0:
-#         bbox = ( np.nanmin(lat2), np.nanmax(lat2), np.nanmin(lon2), np.nanmax(lon2) )
-
-#    print("geocode bounding box:")
-#    print("south: {}".format(bbox[0]))
-#    print("north: {}".format(bbox[1]))
-#    print("west: {}".format(bbox[2]))
-#    print("east: {}".format(bbox[3]))
-
 
     latlon = np.zeros((length*width, 2), dtype=np.float64)
-    lat=lat.reshape(length*width)
-    lon=lon.reshape(length*width)
-    diffll=np.abs(lat2-lat2m)
-    diffll[np.isnan(diffll)]=0
-    lat2[lat2==0]=np.nan   
-    mask= (diffll>0.1) | (np.isnan(lat2)) | ( np.isnan(lat2m)) | ( np.isnan(bands[0].reshape(length*width)))
-    lat=lat[mask==0] 
-    lon=lon[mask==0] 
-    if inps.latMin==0 and inps.latMax==0 and inps.lonMin==0 and inps.lonMax==0:
-         bbox = ( np.nanmin(lat), np.nanmax(lat), np.nanmin(lon), np.nanmax(lon) )
-    print("geocode bounding box:")
-    print("south: {}".format(bbox[0]))
-    print("north: {}".format(bbox[1]))
-    print("west: {}".format(bbox[2]))
-    print("east: {}".format(bbox[3]))
-
-    if inps.latMin!=0 and inps.latMax!=0 and inps.lonMin!=0 and inps.lonMax!=0:
-
-        submask= (lat>bbox[0]) & (lat<bbox[1]) & (lon<bbox[3]) & (lon>bbox[2])
-        
-        lat=lat[submask]
-        lon=lon[submask]
-        print("cropping image {}-> {}".format(len(mask),len(lat)))
-        
-
-
-
-    latlon = np.zeros( (len(lat), 2), dtype=np.float64)
-    latlon[:, 0] = lat
-    latlon[:, 1] = lon
-    bands2=np.zeros( (nbands, len(lat)), dtype=np.float32)
-    for i in range(nbands):
-       temp=bands[i].reshape(length*width)
-       temp=temp[mask==0]
-       if inps.latMin!=0 and inps.latMax!=0 and inps.lonMin!=0 and inps.lonMax!=0:
-           temp=temp[submask]
-       bands2[i]=temp
+    latlon[:, 0] = lat.reshape(length*width)
+    latlon[:, 1] = lon.reshape(length*width)
     #                             n       s                         w       e
     grid_lat, grid_lon = np.mgrid[bbox[1]:bbox[0]:-sample_size_lat, bbox[2]:bbox[3]:sample_size_lon]
-    print("geocoded image dimension {} {}".format(np.shape(grid_lat)[0],np.shape(grid_lat)[1]))
-    msk=np.zeros([length,width])
-    msk[0:2,:]=1
-    msk[-2:,:]=1
-    msk[:,-2:]=1
-    msk[:,0:2]=1
-    msk= msk.reshape(length*width)
-    msk=msk[mask==0]
-    if inps.latMin!=0 and inps.latMax!=0 and inps.lonMin!=0 and inps.lonMax!=0:
-       msk=msk[submask]
-    geomsk = griddata(latlon, msk, (grid_lat, grid_lon), method=rmethod, fill_value = 0.0)
+
     print("interpolate input data")
     bands_geo = []
     for i in range(nbands):
-        geoband = griddata(latlon, (bands2[i]), (grid_lat, grid_lon), method=rmethod, fill_value = 0.0)
-        #geoband = griddata(latlon, (bands[i]).reshape(length*width), (grid_lat, grid_lon), method=rmethod, fill_value = 0.0)
-#        bands_geo.append(geoband)
-        bands_geo.append(np.multiply(geoband,(geomsk==0) ))
+        geoband = griddata(latlon, (bands[i]).reshape(length*width), (grid_lat, grid_lon), method=rmethod, fill_value = 0.0)
+        bands_geo.append(geoband)
 
     print("write result")
     (length_geo, width_geo) = geoband.shape
